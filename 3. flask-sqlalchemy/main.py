3abd3e2ec4d45ff9caeafacbf46568dd62d3271a
Мария К <kvitka.maru@yandex.ru>
2022-03-03 16:57:24 +0300

    егистрация выполнена

--- 3. flask-sqlalchemy/main.py
+++ 3. flask-sqlalchemy/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
@@ -16,10 +16,28 @@ def index():
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def login():
+def reqister():
     form = RegisterForm()
     if form.validate_on_submit():
-        return render_template('success.html', title='Успешно', data=form.login)
+        if form.password.data != form.confirm.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.login.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            email=form.login.data,
+            hashed_password=form.password.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
     return render_template('register.html', title='Регистрация', form=form)
 
 
